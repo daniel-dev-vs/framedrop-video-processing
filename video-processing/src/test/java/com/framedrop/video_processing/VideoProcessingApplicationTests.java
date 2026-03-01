58c7aefd8b2edf7e702bbf3f05b7dcea0237dd4a
Daniel Ferreira <daniel.ferreira.dev@gmail.com>
2026-03-01 13:23:46 -0300

    feat: initial commit

---
+++ video-processing/src/test/java/com/framedrop/video_processing/VideoProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.framedrop.video_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
